e21b5666dd19cb89c1e073ea85a8eee0767c2bc9
bhoover10001 <bhoover@acteva.com>
2015-01-29 12:12:49 -0800

    Update the javascript routines to be a little more efficient.  Added in sliders and progress message.

---
+++ _posts/2015-01-29-medianFilter.md
@@ -0,0 +1,101 @@
+---
+layout: page
+title: Median Filter
+date: 29-Jan-2015
+---
+This is a median filter, that looks for the median of a 3x3 block for each pixel.
+<canvas id="original" />
+
+The noise density - This is the percentage of the image that is covered in white noise.<br />
+<input id="noiseDensity" 
+  value = "0.1"
+  data-slider-id='ex1Slider' 
+  type="text" 
+  data-slider-min="0" 
+  data-slider-max="0.4" 
+  data-slider-step="0.005" 
+  data-slider-value="0.1"/>&nbsp;
+  <input id="go" type="submit" value="Go" />
+  
+<div>
+  The noisy image<br />
+</div>
+<canvas id="greyScale" />
+
+  
+<div>
+  The Median Filtered<br />
+</div>
+
+<canvas id="medianAveraging" />
+
+
+{% highlight JavaScript %}  
+function medianFilterGreyScale(canvas) {
+  var map = getContext(canvas).getImageData(0,0, canvas.width, canvas.height);
+  var pixels = map.data;
+  var window = 1;
+  for (var y = 0; y < canvas.height; y += 1) {
+    for (var x = 0; x < canvas.width; x += 1) {
+      var startY = Math.max(0, y - window);
+      var startX = Math.max(0, x - window);
+      var endY = Math.min(canvas.height -1, y + window);
+      var endX = Math.min(canvas.width -1, x + window);
+      var greyScalePixels = [];
+      for (var y1 = startY; y1 <= endY; y1 += 1) {
+         for (var x1 = startX; x1 <= endX; x1 +=1) {
+           greyScalePixels.push(pixels[(y1 * canvas.width * 4) + x1 * 4]);
+         }
+      }
+      setGreyPixels(pixels, currentPixelArrayLocation(y, x, canvas.width), median(greyScalePixels));
+    }
+  }
+  getContext(canvas).putImageData(map, 0, 0);
+}
+
+function setGreyPixels(pixels, arrayLocation, value) {
+  pixels[arrayLocation] = pixels[arrayLocation + 1] = pixels[arrayLocation + 2] = value;
+}
+
+function mean(pixelArray) {
+  var total = pixelArray.reduce(function(a,b) {return a+b});
+  return Math.floor(total/pixelArray.length);
+}
+{% endhighlight %}
+
+<script src="{{ page.base_url }}/assets/js/image.js"></script>
+<script>
+  var img = new Image();   // Create new img element
+  var originalCanvas = document.getElementById('original');
+  var greyScaleCanvas = document.getElementById('greyScale');
+  var medianCanvas = document.getElementById('medianAveraging');
+  img.addEventListener("load", function() {
+    setToCanvas(img, originalCanvas);
+    convertToGreyScale(originalCanvas);
+     generateImages($('#noiseDensity').val());
+  }, false);
+  
+  img.src = '{{ page.base_url }}/img/Ferocious_Tammy.png'; // Set source path
+  
+  function generateImages(noiseDensity) {
+    setToCanvas(img, greyScaleCanvas);
+    convertToGreyScale(greyScaleCanvas);
+    greyScaleWhiteNoiseGenerator(greyScaleCanvas, noiseDensity);
+    setToCanvas(img, medianCanvas);
+    convertToGreyScale(medianCanvas);
+    greyScaleWhiteNoiseGenerator(medianCanvas, noiseDensity);
+    medianFilterGreyScale(medianCanvas);
+  }
+  
+  $('#go').click(function() {
+    generateImages($('#noiseDensity').val());
+  });
+  
+  $(function() {
+    $('#noiseDensity').slider({
+      formatter: function(value) {
+        return 'Current value: ' + value;
+      }
+    });
+  });
+</script>
--- assets/js/image.js
+++ assets/js/image.js
@@ -70,22 +70,89 @@ function localMeansGreyScale(canvas, blockSize) {
   var map = getContext(canvas).getImageData(0,0, canvas.width, canvas.height);
   var pixels = map.data;
   var window = Math.floor(blockSize /2);
+  if (blockSize > 1) {
+    for (var y = 0; y < canvas.height; y += 1) {
+      for (var x = 0; x < canvas.width; x += 1) {
+        var startY = Math.max(0, y - window);
+        var startX = Math.max(0, x - window);
+        var endY = Math.min(canvas.height -1, y + window);
+        var endX = Math.min(canvas.width -1, x + window);
+        var greyScalePixels = [];
+        for (var y1 = startY; y1 <= endY; y1 += 1) {
+           for (var x1 = startX; x1 <= endX; x1 +=1) {
+             greyScalePixels.push(pixels[(y1 * canvas.width * 4) + x1 * 4]);
+           }
+        }
+        setGreyPixels(pixels, currentPixelArrayLocation(y, x, canvas.width), mean(greyScalePixels));
+      }
+    }
+  }
+  getContext(canvas).putImageData(map, 0, 0);
+}
+
+function medianFilterGreyScale(canvas) {
+  var map = getContext(canvas).getImageData(0,0, canvas.width, canvas.height);
+  var pixels = map.data;
+  var window = 1;
   for (var y = 0; y < canvas.height; y += 1) {
     for (var x = 0; x < canvas.width; x += 1) {
       var startY = Math.max(0, y - window);
       var startX = Math.max(0, x - window);
-      var endY = Math.min(canvas.height, y + window);
-      var endX = Math.min(canvas.width, x + window);
-      var greyScaleAverage = 0;
-      for (var y1 = startY; y1 < endY; y1 += 1) {
-         for (var x1 = startX; x1 < endX; x1 +=1) {
-           greyScaleAverage += pixels[(y1 * canvas.width * 4) + x1 * 4];
+      var endY = Math.min(canvas.height -1, y + window);
+      var endX = Math.min(canvas.width -1, x + window);
+      var greyScalePixels = [];
+      for (var y1 = startY; y1 <= endY; y1 += 1) {
+         for (var x1 = startX; x1 <= endX; x1 +=1) {
+           greyScalePixels.push(pixels[(y1 * canvas.width * 4) + x1 * 4]);
          }
       }
-      greyScaleAverage = greyScaleAverage / ((endX - startX) * (endY - startY));
-      var currentRedPixel = (y * canvas.width * 4) + x * 4;
-      pixels[currentRedPixel] = pixels[currentRedPixel + 1] = pixels[currentRedPixel + 2] = greyScaleAverage;
+      setGreyPixels(pixels, currentPixelArrayLocation(y, x, canvas.width), median(greyScalePixels));
     }
   }
   getContext(canvas).putImageData(map, 0, 0);
+}
+
+function setGreyPixels(pixels, arrayLocation, value) {
+  pixels[arrayLocation] = pixels[arrayLocation + 1] = pixels[arrayLocation + 2] = value;
+}
+
+function mean(pixelArray) {
+  var total = pixelArray.reduce(function(a,b) {return a+b});
+  return Math.floor(total/pixelArray.length);
+}
+
+function median(pixelArray) {
+  pixelArray.sort(function(a,b) {return a - b;});
+  var half = Math.floor(pixelArray.length/2);
+  if (pixelArray.length % 2) {
+    return pixelArray[half];
+  }
+  return pixelArray[half-1];
+}
+
+/**
+ * Generates a random noise, with a density, that really the probability that any 
+ * pixel is noisy.  This adds the exact same noise to all the RGB channels, hence the "greyScale" label.
+ * Density should be between 0 and 1.
+ * @param canvas
+ * @param density
+ */
+function greyScaleWhiteNoiseGenerator(canvas, density) {
+  var map = getContext(canvas).getImageData(0,0, canvas.width, canvas.height);
+  var pixels = map.data;
+  for (var y = 0; y < canvas.height; y += 1) {
+    for (var x = 0; x < canvas.width; x += 1) {
+      if (Math.random() > density) {
+        continue;
+      }
+      var noise = Math.floor(Math.random() * 256);
+      var currentRedPixel = currentPixelArrayLocation(y, x, canvas.width);
+      pixels[currentRedPixel] = pixels[currentRedPixel + 1] = pixels[currentRedPixel + 2] = (pixels[currentRedPixel] + noise) % 256;
+    }
+  }
+  getContext(canvas).putImageData(map, 0, 0);
+}
+
+function currentPixelArrayLocation(y, x, imageWidth) {
+  return (y * imageWidth * 4) + x * 4;
 }
